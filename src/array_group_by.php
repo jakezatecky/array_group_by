c0ef89871615d2b0fab2b7a4d9ec22ac24d2a239
Jake Zatecky <jake.zatecky@gmail.com>
2017-06-30 18:05:10 -0400

    Let's be fully PSR-2 compliant

--- src/array_group_by.php
+++ src/array_group_by.php
@@ -13,29 +13,29 @@
  */
 function array_group_by($arr, $key)
 {
-	if (!is_array($arr)) {
-		trigger_error('array_group_by(): The first argument should be an array', E_USER_ERROR);
-	}
-	if (!is_string($key) && !is_int($key) && !is_float($key)) {
-		trigger_error('array_group_by(): The key should be a string or an integer', E_USER_ERROR);
-	}
+    if (!is_array($arr)) {
+        trigger_error('array_group_by(): The first argument should be an array', E_USER_ERROR);
+    }
+    if (!is_string($key) && !is_int($key) && !is_float($key)) {
+        trigger_error('array_group_by(): The key should be a string or an integer', E_USER_ERROR);
+    }
 
-	// Load the new array, splitting by the target key
-	$grouped = [];
-	foreach ($arr as $value) {
-		$grouped[$value[$key]][] = $value;
-	}
+    // Load the new array, splitting by the target key
+    $grouped = [];
+    foreach ($arr as $value) {
+        $grouped[$value[$key]][] = $value;
+    }
 
-	// Recursively build a nested grouping if more parameters are supplied
-	// Each grouped array value is grouped according to the next sequential key
-	if (func_num_args() > 2) {
-		$args = func_get_args();
+    // Recursively build a nested grouping if more parameters are supplied
+    // Each grouped array value is grouped according to the next sequential key
+    if (func_num_args() > 2) {
+        $args = func_get_args();
 
-		foreach ($grouped as $key => $value) {
-			$params = array_merge([$value], array_slice($args, 2, func_num_args()));
-			$grouped[$key] = call_user_func_array('array_group_by', $params);
-		}
-	}
+        foreach ($grouped as $key => $value) {
+            $params = array_merge([$value], array_slice($args, 2, func_num_args()));
+            $grouped[$key] = call_user_func_array('array_group_by', $params);
+        }
+    }
 
-	return $grouped;
+    return $grouped;
 }
--- tests/ArrayGroupTest.php
+++ tests/ArrayGroupTest.php
@@ -4,211 +4,211 @@ use PHPUnit\Framework\TestCase;
 
 class ArrayGroupTest extends TestCase
 {
-	protected $states;
+    protected $states;
 
-	protected $numbers;
+    protected $numbers;
 
-	public function setUp()
-	{
-		$this->states = [
-			[
-				'state'  => 'IN',
-				'city'   => 'Indianapolis',
-				'object' => 'School bus',
-			],
-			[
-				'state'  => 'IN',
-				'city'   => 'Indianapolis',
-				'object' => 'Manhole',
-			],
-			[
-				'state'  => 'IN',
-				'city'   => 'Plainfield',
-				'object' => 'Basketball',
-			],
-			[
-				'state'  => 'CA',
-				'city'   => 'San Diego',
-				'object' => 'Light bulb',
-			],
-			[
-				'state'  => 'CA',
-				'city'   => 'Mountain View',
-				'object' => 'Space pen',
-			],
-		];
+    public function setUp()
+    {
+        $this->states = [
+            [
+                'state'  => 'IN',
+                'city'   => 'Indianapolis',
+                'object' => 'School bus',
+            ],
+            [
+                'state'  => 'IN',
+                'city'   => 'Indianapolis',
+                'object' => 'Manhole',
+            ],
+            [
+                'state'  => 'IN',
+                'city'   => 'Plainfield',
+                'object' => 'Basketball',
+            ],
+            [
+                'state'  => 'CA',
+                'city'   => 'San Diego',
+                'object' => 'Light bulb',
+            ],
+            [
+                'state'  => 'CA',
+                'city'   => 'Mountain View',
+                'object' => 'Space pen',
+            ],
+        ];
 
-		$this->numbers = [
-			[1, 'Only a cat of a different coat'],
-			[1, 'That\'s all the truth I know'],
-			[2, 'That I must bow so low'],
-		];
-	}
+        $this->numbers = [
+            [1, 'Only a cat of a different coat'],
+            [1, 'That\'s all the truth I know'],
+            [2, 'That I must bow so low'],
+        ];
+    }
 
-	public function testGroupStringFirstLevel()
-	{
-		$expected = ['IN', 'CA'];
+    public function testGroupStringFirstLevel()
+    {
+        $expected = ['IN', 'CA'];
 
-		$this->assertEquals($expected, array_keys(array_group_by($this->states, 'state')));
-	}
+        $this->assertEquals($expected, array_keys(array_group_by($this->states, 'state')));
+    }
 
-	public function testGroupByState()
-	{
-		$expected = [
-			'IN' => [
-				[
-					'state'  => 'IN',
-					'city'   => 'Indianapolis',
-					'object' => 'School bus',
-				],
-				[
-					'state'  => 'IN',
-					'city'   => 'Indianapolis',
-					'object' => 'Manhole',
-				],
-				[
-					'state'  => 'IN',
-					'city'   => 'Plainfield',
-					'object' => 'Basketball',
-				],
-			],
-			'CA' => [
-				[
-					'state'  => 'CA',
-					'city'   => 'San Diego',
-					'object' => 'Light bulb',
-				],
-				[
-					'state'  => 'CA',
-					'city'   => 'Mountain View',
-					'object' => 'Space pen',
-				],
-			],
-		];
+    public function testGroupByState()
+    {
+        $expected = [
+            'IN' => [
+                [
+                    'state'  => 'IN',
+                    'city'   => 'Indianapolis',
+                    'object' => 'School bus',
+                ],
+                [
+                    'state'  => 'IN',
+                    'city'   => 'Indianapolis',
+                    'object' => 'Manhole',
+                ],
+                [
+                    'state'  => 'IN',
+                    'city'   => 'Plainfield',
+                    'object' => 'Basketball',
+                ],
+            ],
+            'CA' => [
+                [
+                    'state'  => 'CA',
+                    'city'   => 'San Diego',
+                    'object' => 'Light bulb',
+                ],
+                [
+                    'state'  => 'CA',
+                    'city'   => 'Mountain View',
+                    'object' => 'Space pen',
+                ],
+            ],
+        ];
 
-		$this->assertEquals($expected, array_group_by($this->states, 'state'));
-	}
+        $this->assertEquals($expected, array_group_by($this->states, 'state'));
+    }
 
-	public function testGroupByStateCity()
-	{
-		$expected = [
-			'IN' => [
-				'Indianapolis' => [
-					[
-						'state'  => 'IN',
-						'city'   => 'Indianapolis',
-						'object' => 'School bus',
-					],
-					[
-						'state'  => 'IN',
-						'city'   => 'Indianapolis',
-						'object' => 'Manhole',
-					],
-				],
-				'Plainfield'   => [
-					[
-						'state'  => 'IN',
-						'city'   => 'Plainfield',
-						'object' => 'Basketball',
-					],
-				],
-			],
-			'CA' => [
-				'San Diego'     => [
-					[
-						'state'  => 'CA',
-						'city'   => 'San Diego',
-						'object' => 'Light bulb',
-					],
-				],
-				'Mountain View' => [
-					[
-						'state'  => 'CA',
-						'city'   => 'Mountain View',
-						'object' => 'Space pen',
-					],
-				],
-			],
-		];
+    public function testGroupByStateCity()
+    {
+        $expected = [
+            'IN' => [
+                'Indianapolis' => [
+                    [
+                        'state'  => 'IN',
+                        'city'   => 'Indianapolis',
+                        'object' => 'School bus',
+                    ],
+                    [
+                        'state'  => 'IN',
+                        'city'   => 'Indianapolis',
+                        'object' => 'Manhole',
+                    ],
+                ],
+                'Plainfield'   => [
+                    [
+                        'state'  => 'IN',
+                        'city'   => 'Plainfield',
+                        'object' => 'Basketball',
+                    ],
+                ],
+            ],
+            'CA' => [
+                'San Diego'     => [
+                    [
+                        'state'  => 'CA',
+                        'city'   => 'San Diego',
+                        'object' => 'Light bulb',
+                    ],
+                ],
+                'Mountain View' => [
+                    [
+                        'state'  => 'CA',
+                        'city'   => 'Mountain View',
+                        'object' => 'Space pen',
+                    ],
+                ],
+            ],
+        ];
 
-		$this->assertEquals($expected, array_group_by($this->states, 'state', 'city'));
-	}
+        $this->assertEquals($expected, array_group_by($this->states, 'state', 'city'));
+    }
 
-	public function testGroupByInt()
-	{
-		$expected = [
-			1 => [
-				[1, 'Only a cat of a different coat'],
-				[1, 'That\'s all the truth I know'],
-			],
-			2 => [
-				[2, 'That I must bow so low'],
-			],
-		];
+    public function testGroupByInt()
+    {
+        $expected = [
+            1 => [
+                [1, 'Only a cat of a different coat'],
+                [1, 'That\'s all the truth I know'],
+            ],
+            2 => [
+                [2, 'That I must bow so low'],
+            ],
+        ];
 
-		$this->assertEquals($expected, array_group_by($this->numbers, 0));
-	}
+        $this->assertEquals($expected, array_group_by($this->numbers, 0));
+    }
 
-	public function testGroupTwoLevels()
-	{
-		$expected = [
-			'IN' => [
-				'Indianapolis' => [
-					[
-						'state'  => 'IN',
-						'city'   => 'Indianapolis',
-						'object' => 'School bus',
-					],
-					[
-						'state'  => 'IN',
-						'city'   => 'Indianapolis',
-						'object' => 'Manhole',
-					],
-				],
-				'Plainfield'   => [
-					[
-						'state'  => 'IN',
-						'city'   => 'Plainfield',
-						'object' => 'Basketball',
-					],
-				],
-			],
-			'CA' => [
-				'San Diego'     => [
-					[
-						'state'  => 'CA',
-						'city'   => 'San Diego',
-						'object' => 'Light bulb',
-					],
-				],
-				'Mountain View' => [
-					[
-						'state'  => 'CA',
-						'city'   => 'Mountain View',
-						'object' => 'Space pen',
-					],
-				],
-			],
-		];
+    public function testGroupTwoLevels()
+    {
+        $expected = [
+            'IN' => [
+                'Indianapolis' => [
+                    [
+                        'state'  => 'IN',
+                        'city'   => 'Indianapolis',
+                        'object' => 'School bus',
+                    ],
+                    [
+                        'state'  => 'IN',
+                        'city'   => 'Indianapolis',
+                        'object' => 'Manhole',
+                    ],
+                ],
+                'Plainfield'   => [
+                    [
+                        'state'  => 'IN',
+                        'city'   => 'Plainfield',
+                        'object' => 'Basketball',
+                    ],
+                ],
+            ],
+            'CA' => [
+                'San Diego'     => [
+                    [
+                        'state'  => 'CA',
+                        'city'   => 'San Diego',
+                        'object' => 'Light bulb',
+                    ],
+                ],
+                'Mountain View' => [
+                    [
+                        'state'  => 'CA',
+                        'city'   => 'Mountain View',
+                        'object' => 'Space pen',
+                    ],
+                ],
+            ],
+        ];
 
-		$this->assertEquals($expected, array_group_by($this->states, 'state', 'city'));
-	}
+        $this->assertEquals($expected, array_group_by($this->states, 'state', 'city'));
+    }
 
-	/**
-	 * @expectedException \PHPUnit\Framework\Error\Error
-	 * @expectedExceptionMessage array_group_by(): The first argument should be an array
-	 */
-	public function testArrayError()
-	{
-		array_group_by(null, null);
-	}
+    /**
+     * @expectedException \PHPUnit\Framework\Error\Error
+     * @expectedExceptionMessage array_group_by(): The first argument should be an array
+     */
+    public function testArrayError()
+    {
+        array_group_by(null, null);
+    }
 
-	/**
-	 * @expectedException \PHPUnit\Framework\Error\Error
-	 * @expectedExceptionMessage array_group_by(): The key should be a string or an integer
-	 */
-	public function testKeyError()
-	{
-		array_group_by([], null);
-	}
+    /**
+     * @expectedException \PHPUnit\Framework\Error\Error
+     * @expectedExceptionMessage array_group_by(): The key should be a string or an integer
+     */
+    public function testKeyError()
+    {
+        array_group_by([], null);
+    }
 }
